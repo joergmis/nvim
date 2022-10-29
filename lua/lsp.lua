caeb1caa6c2b0428d1e1b346d6c851a025fbed32
Mischa Jörg <mischa@ninetyfour.ch>
2022-10-29 19:00:51 +0200

    feat: hover + signature preview

--- lua/lsp.lua
+++ lua/lsp.lua
@@ -1,52 +1,55 @@
-require'lspconfig'.angularls.setup{}
-require'lspconfig'.ansiblels.setup{}
-require'lspconfig'.bashls.setup{}
-require'lspconfig'.dockerls.setup{}
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
-require'lspconfig'.eslint.setup{}
+require 'lspconfig'.angularls.setup { capabilities = capabilities }
+require 'lspconfig'.ansiblels.setup { capabilities = capabilities }
+require 'lspconfig'.bashls.setup { capabilities = capabilities }
+require 'lspconfig'.dockerls.setup { capabilities = capabilities }
+
+require 'lspconfig'.eslint.setup { capabilities = capabilities }
 -- autocmd BufWritePre *.tsx,*.ts,*.jsx,*.js EslintFixAll
 
-require'lspconfig'.golangci_lint_ls.setup{}
-require'lspconfig'.gopls.setup{}
+require 'lspconfig'.golangci_lint_ls.setup { capabilities = capabilities }
+require 'lspconfig'.gopls.setup { capabilities = capabilities }
 
-require'lspconfig'.html.setup{}
-require'lspconfig'.jsonls.setup{}
+require 'lspconfig'.html.setup { capabilities = capabilities }
+require 'lspconfig'.jsonls.setup { capabilities = capabilities }
 
-require'lspconfig'.rust_analyzer.setup{}
-require'lspconfig'.tailwindcss.setup{}
-require'lspconfig'.tsserver.setup{}
+require 'lspconfig'.rust_analyzer.setup { capabilities = capabilities }
+require 'lspconfig'.tailwindcss.setup { capabilities = capabilities }
+require 'lspconfig'.tsserver.setup { capabilities = capabilities }
 
 require('lspconfig').yamlls.setup {
-  settings = {
-    yaml = {
-      schemas = {},
-    },
-  }
+    capabilities = capabilities,
+    settings = {
+        yaml = {
+            schemas = {},
+        },
+    }
 }
 
-require'lspconfig'.sumneko_lua.setup {
-  settings = {
-    Lua = {
-      runtime = {
-        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-        version = 'LuaJIT',
-      },
-      diagnostics = {
-        -- Get the language server to recognize the `vim` global
-        globals = {'vim'},
-      },
-      workspace = {
-        -- Make the server aware of Neovim runtime files
-        library = vim.api.nvim_get_runtime_file("", true),
-      },
-      -- Do not send telemetry data containing a randomized but unique identifier
-      telemetry = {
-        enable = false,
-      },
+require 'lspconfig'.sumneko_lua.setup {
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = 'LuaJIT',
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { 'vim' },
+            },
+            workspace = {
+                -- Make the server aware of Neovim runtime files
+                library = vim.api.nvim_get_runtime_file("", true),
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
     },
-  },
 }
 
 -- format on save
 vim.cmd [[autocmd BufWritePre * lua vim.lsp.buf.formatting_sync()]]
-
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -39,6 +39,33 @@ require('packer').startup(function(use)
     use 'kyazdani42/nvim-web-devicons'
     use 'nvim-lua/plenary.nvim'
 
+    use {
+        "lewis6991/hover.nvim",
+        config = function()
+            require("hover").setup {
+                init = function()
+                    -- Require providers
+                    require("hover.providers.lsp")
+                    -- require('hover.providers.gh')
+                    -- require('hover.providers.jira')
+                    -- require('hover.providers.man')
+                    -- require('hover.providers.dictionary')
+                end,
+                preview_opts = {
+                    border = nil
+                },
+                -- Whether the contents of a currently open hover window should be moved
+                -- to a :h preview-window when pressing the hover keymap.
+                preview_window = false,
+                title = true
+            }
+
+            -- Setup keymaps
+            vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+            vim.keymap.set("n", "gK", require("hover").hover_select, { desc = "hover.nvim (select)" })
+        end
+    }
+
     -- zen mode
     use {
         'folke/zen-mode.nvim',
@@ -104,8 +131,8 @@ cmp.setup({
         end,
     },
     window = {
-        -- completion = cmp.config.window.bordered(),
-        -- documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
     },
     mapping = cmp.mapping.preset.insert({
         ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -151,3 +178,5 @@ cmp.setup.cmdline(':', {
         { name = 'cmdline' }
     })
 })
+
+require('hover.providers.lsp')
