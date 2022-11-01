4c8cc7ef24607878b8b705eb8a8b628d79d02553
Mischa Jörg <mischa@ninetyfour.ch>
2022-11-01 15:38:49 +0100

    feat: correct snippet engine, add vim-go

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -5,6 +5,8 @@ require('packer').startup(function(use)
     -- colorscheme
     use 'lunarvim/Onedarker.nvim'
 
+    use 'fatih/vim-go'
+
     -- fuzzy search
     use 'nvim-telescope/telescope.nvim'
     use { 'nvim-telescope/telescope-fzf-native.nvim',
@@ -120,12 +122,8 @@ require('nvim-treesitter.configs').setup {
 local cmp = require 'cmp'
 cmp.setup({
     snippet = {
-        -- REQUIRED - you must specify a snippet engine
         expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-            -- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+            require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
         end,
     },
     window = {
